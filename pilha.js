da2f6a68c7119bb4b21854ebb75066b43b620749
Jaime Almeida <jaime.v.jaime@gmail.com>
2025-05-05 17:08:57 -0300

    Create pilha.js

---
+++ pilha.js
@@ -0,0 +1,60 @@
+// Classe Pilha
+class Pilha {
+  constructor() {
+    this.itens = [];
+  }
+
+  empilhar(item) {
+    this.itens.push(item);
+    console.log(`✔️ Elemento '${item}' empilhado.`);
+  }
+
+  desempilhar() {
+    if (this.estaVazia()) {
+      console.log('⚠️ A pilha está vazia. Nada a desempilhar.');
+      return null;
+    }
+    const removido = this.itens.pop();
+    console.log(`❌ Elemento '${removido}' desempilhado.`);
+    return removido;
+  }
+
+  topo() {
+    if (this.estaVazia()) {
+      return null;
+    }
+    return this.itens[this.itens.length - 1];
+  }
+
+  estaVazia() {
+    return this.itens.length === 0;
+  }
+
+  tamanho() {
+    return this.itens.length;
+  }
+
+  limpar() {
+    this.itens = [];
+    console.log('🧹 Pilha esvaziada.');
+  }
+
+  imprimir() {
+    console.log('📦 Pilha atual:', this.itens.slice().reverse().join(' <- topo'));
+  }
+}
+
+// DEMONSTRAÇÃO
+
+const pilha = new Pilha();
+pilha.empilhar('Livro A');
+pilha.empilhar('Livro B');
+pilha.empilhar('Livro C');
+pilha.empilhar('Livro D');
+console.log('📏 Topo da pilha:' + pilha.topo());
+console.log('📏 Tamanho da pilha:' + pilha.tamanho());
+pilha.desempilhar();
+pilha.limpar();
+
+pilha.imprimir();
+
